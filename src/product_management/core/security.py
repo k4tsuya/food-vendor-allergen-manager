258adce4d9262a7f40f78165984cb313a444d8f1
Liang Yi Huang <k4tsuya@gmail.com>
2026-07-31 07:42:21 +0000

    added SECRET_KEY check

--- src/product_management/core/security.py
+++ src/product_management/core/security.py
@@ -23,6 +23,9 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
+if not SECRET_KEY:
+    raise RuntimeError("SECRET_KEY not set in .env")
+
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
